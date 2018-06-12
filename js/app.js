7d312734c98c3a0b04e7d665395b9b64f0880796
Shiva <dgpshiva@gmail.com>
2018-06-11 22:58:04 -0500

    Adding cat levels

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var ViewModel = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable("Tabby");
+    this.imgSrc = ko.observable("img/22252709_010df3379e_z.jpg");
+
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
+
+    this.catLevel = ko.computed(function() {
+        if (this.clickCount() >=0 && this.clickCount() <= 10) {
+            return "Infant";
+        }
+        else if (this.clickCount() >= 10 && this.clickCount() <= 30 ){
+            return "New Born";
+        }
+        else {
+            return "Pro";
+        }
+    }, this);
+}
+
+ko.applyBindings(new ViewModel());
